3805ea8ebdc2600294266a954a99f14ef0f2d079
gigadot <gigadot@gmail.com>
2022-07-01 11:15:29 +0700

    Added tracing example

---
+++ lessons/10/tracing/api-service/src/main/java/com/example/demo/ErrorHandler.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import io.opentelemetry.api.trace.Span;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+  private Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
+
+  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(Exception.class)
+  @ResponseBody
+  public String handleInternalError(Exception e) {
+    logger.error("internal server error", e);
+    return String.format("Internal Server Error (traceId: %s)", MDC.get("traceId"));
+  }
+
+}
